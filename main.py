af35265f1f82e1511cc6db160826027895e2bb2e
gobigamani-17 <gobigamanimk@gmail.com>
2026-09-01 14:40:57 +0530

    np2

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-# file=open("student.txt","w")
-# file.write("hello python")
-# file.close()
+print("hello")
 
-# file=open("student.txt","r")
-# data=file.read()
-# print(data)
- 
-file=open("student name.txt","w")
-file.write("priyanka\n""jayarani\n""ezhil\n""rexc\n")
-file.close()
-file=open("student name.txt","r")
-data=file.read()
-print(data)
-file.close()
 
-# try:
-#     a=10
-#     b=20
-#     print(a+b)
-# except Exception as e:
-#     print(e)
-# finally:
-#     print("program Ended")
+
